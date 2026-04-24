54628af9ccf417c52f39e7dd3486b202f98f15e2
Diogo-Almeida-Costa <jdiogoalmeida988@gmail.com>
2026-04-23 22:07:23 -0300

    feat(auth): implementar geração de token JWT e endpoint de login

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"cinefinder/internal/service"
+)
+
+type LoginRequest struct{
+	Email string `json:email`
+	Password string `json:password`
+}
+
+func LoginHandler(authService *service.AuthService, userService *service.UserService) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		var req LoginRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Requisição inválida", http.StatusBadRequest)
+			return
+		}
+
+		user, err := userService.ValidateUser(req.Email, req.Password)
+		if err != nil{
+			http.Error(w, "Credenciais inválidas", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := authService.GenerateToken(*user)
+		if err != nil{
+			http.Error(w, "Erro ao gerar token", http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(map[string]string{"token":token})
+	}
+}
--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"time"
+	"github.com/golang-jwt/jwt/v5"
+	"cinefinder/internal/model"
+)
+
+var jwtKey = []byte("chave_secreta")
+
+func (s *AuthService) GenerateToken(user model.User) (string, error){
+	expirationTime := time.Now().add(24 * time.Hour)
+	claims := &jwt.MapClaims{
+		"user_id": user.ID,
+		"email": user.Email,
+		"exp": expirationTime.Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigninMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
