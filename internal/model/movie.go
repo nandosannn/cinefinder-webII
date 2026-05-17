1a75e5e5f5bdc7f733538b9119800d857679a469
Fernando Santos <158214975+nandosannn@users.noreply.github.com>
2026-05-17 17:46:09 -0300

    fix: correção no model de movie

--- internal/model/movie.go
+++ internal/model/movie.go
@@ -1,11 +1,10 @@
 package model
 
 type Movie struct {
-	ID           int    `json:"id"`
-	Title        string `json:"title"`
-	Director     string `json:"director"`
-	Year         int    `json:"year"`
-	Genre        string `json:"genre"`
-	Available    bool   `json:"available"`
-	
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Director  string `json:"director"`
+	Year      int    `json:"year"`
+	Genre     string `json:"genre"`
+	Available int    `json:"available_copies"`
 }
