81c878617dc00fc6bbd77a7b7d013644c3830d06
JoBernardoLB <josepontobernardo@gmail.com>
2026-05-22 12:16:44 -0300

    Seed para popular movies

---
+++ internal/db/seeds/001_movies.sql
@@ -0,0 +1,72 @@
+INSERT INTO movies 
+(
+    title, 
+    director, 
+    release_year, 
+    genre, 
+    available_copies
+)
+VALUES
+(
+    'Transformers',
+    'Michael Bay, Travis Knight, Steven Caple Jr.',
+    2007,
+    'Sci-Fi',
+    5
+),
+(
+    'Inception',
+    'Christopher Nolan',
+    2010,
+    'Sci-Fi',
+    3
+),
+(
+    'The Batman',
+    'Matt Reeves',
+    2022,
+    'Action',
+    7
+)
+(
+    'Sinners',
+    'Ryan Coogler',
+    2025,
+    'Horror/Action',
+    2
+)
+(
+    'Matrix',
+    'The Wachowskis',
+    1999,
+    'Sci-Fi',
+    4
+)
+(
+    'Matrix Reloaded',
+    'The Wachowskis',
+    2003,
+    'Sci-Fi',
+    10
+)
+(
+    'Pulp Fiction: Tempo de Violência',
+    'Quentin Tarantino',
+    1995,
+    'Crime',
+    12
+)
+(
+    'Transformers',
+    'Michael Bay, Travis Knight, Steven Caple Jr.',
+    2007,
+    'Sci-Fi',
+    5
+)
+(
+    'O Agente Secreto',
+    'Kleber Mendonça Filho',
+    2025,
+    'Thriller/Crime',
+    6
+);
